44bc884a79e89bd7199c6669ec71ac6722b5d0db
Suraj260801 <suraj9067026719@gmail.com>
2023-01-24 17:40:49 +0530

    Submitted play store ui Assignment

---
+++ play_store_home_screen/lib/utils/list_item_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListItemData {
+  String name;
+  String rating;
+  String category;
+  Image image;
+
+  ListItemData({
+    required this.name,
+    required this.rating,
+    required this.category,
+    required this.image,
+  });
+}
