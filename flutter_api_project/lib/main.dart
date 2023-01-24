57d43a24f038cd41edc684ded57da69ec878a71f
Suraj260801 <suraj9067026719@gmail.com>
2023-01-24 17:46:34 +0530

    Submitted Flutter Api Assignment

---
+++ flutter_api_project/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_project/pages/homepage.dart';
+import 'package:http/http.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+
+
